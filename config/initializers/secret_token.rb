95359823113ed8fb07998448a8f9af0b497b9833
kennyj <kennyj@gmail.com>
2012-12-17 21:38:54 +0900

    Generate rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhereAreYou::Application.config.secret_key_base = '9b20a5c0568a22414291c470b812d3d0508e2f0d26051d87ddd4ce60cddf675147fa6c9571d3466629b76f08fc3d4709056a8f1a4dab621ba1cff49291a987e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhereAreYou::Application.config.session_store :encrypted_cookie_store, key: '_where_are_you_session'
