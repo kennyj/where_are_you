cc611daf4022fcbff57ec038d1305703c825d0bf
kennyj <kennyj@gmail.com>
2012-12-23 03:44:20 +0900

    Import initial data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# import cities from zip
+City.import!
+
+# load repo / contributors data from github api
+["rails/rails",
+ "jquery/jquery-mobile", "jquery/qunit", "jquery/jquery",
+  "mrdoob/three.js",
+  "rails/turbolinks", "rails/journey", "rails/arel",
+  "rails/strong_parameters", "rails/exception_notification",
+  "rails/coffee-rails", "rails/sass-rails", "rails/sprockets-rails",
+  "rails/jquery-rails", "defunkt/jquery-pjax", "jnicklas/carrierwave",
+  "intridea/omniauth", "pilu/web-app-theme", "LearnBoost/socket.io",
+  "django/django", "nvie/gitflow", "ryanb/cancan",
+  "github/gitignore", "defunkt/resque",
+  "sinatra/sinatra", "antirez/redis", "documentcloud/underscore",
+  "cucumber/cucumber", "Modernizr/Modernizr", "jashkenas/coffee-script",
+  "symfony/symfony", "mislav/will_paginate", "justinfrench/formtastic",
+  "documentcloud/backbone", "jquery/jquery-ui", "binarylogic/authlogic",
+  "visionmedia/express", "thoughtbot/paperclip", "plataformatec/devise",
+  "joshuaclayton/blueprint-css", "joyent/node"].each do |full_name|
+  repo = Repository.create_or_update_from_api(full_name)
+  repo.refresh_contributors_from_api
+end
+
+# Populate lng / lat from google map api.
+Location.all.each { |l| l.coded!} 
